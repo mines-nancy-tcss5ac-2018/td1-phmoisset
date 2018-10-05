d1fba0ef72f5450d0c694d6e63c7c561255ceb56
phmoisset <43882709+phmoisset@users.noreply.github.com>
2018-10-05 21:21:51 +0200

    Update problem16.py

--- problem16.py
+++ problem16.py
@@ -5,10 +5,8 @@ def solve(n):
     """Solve the 16th Euler problem. Return the sum of the digits of 2**n."""
     number = str(2**n)
     sum = 0
-    letter = str()
-    for i in number:
-        letter = int(i)
-        sum+=letter
+    for digit in number: #run through the number 
+        sum+=int(digit) #convert each digit of the string into an integer and sum it
     return sum
 
 assert(solve(15)==26)
