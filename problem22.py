eddb0385e3a5d92f7e6d10bf824367c40546e8a8
phmoisset <43882709+phmoisset@users.noreply.github.com>
2018-10-05 21:52:47 +0200

    Add files via upload

---
+++ problem22.py
@@ -0,0 +1,41 @@
+#  Projet Euler
+## Problem 22
+
+def extract(file):
+    l=[]
+    for line in open(file,'r'): 
+        """ extract a string from a file """
+        l.append(line)
+        """ erase and separate each name, except the first and the last one """
+        p=l[0].split('","') 
+        """ erase the " on the first name """
+        p[0]=p[0].replace('"','') 
+        """ erase the " on the last name """
+        p[-1]=p[-1].replace('"','')
+    return p
+
+def value(name):
+    """return the value of a name"""
+    value = 0
+    for letter in name: #run through the name to give each letter its value
+        value += ord(letter)-64 #ord() gives the ascii number so i had to substract 64 to have the right number
+    return value
+
+assert(value('COLIN') == 53)
+
+
+
+def solve(filepath):
+    """ solve the 22th Euler problem """
+    ans = 0
+    for coef,name in enumerate(sorted(extract(filepath)),1): 
+        #run through the list and give the row of each name
+        #the 1 in enumerate is here to start the row at 1 and not at 0 
+        #which would 'erase' the first name
+        ans += value(name)*coef #the ans is the sum of all the name value * coef whiwh their row
+    return ans
+
+assert(solve('p022_names.txt') == 871198282)
+
+
+    
